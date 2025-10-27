5f9cf01e60c03e3d49bef5bd5c28c5baa4527d26
CrozzDev <crozzdev95@outlook.com>
2025-10-26 23:32:37 -0500

    improved logic for the fibonaccion_sequence challenge

--- freecodecamp/fibonacci_sequence.py
+++ freecodecamp/fibonacci_sequence.py
@@ -1,17 +1,17 @@
-def fibonacci_sequence(start_sequence:list, length:int) -> list:
-    fibonacci_sequence = start_sequence
+def fibonacci_sequence(start_sequence: list, length: int) -> list:
     if length <= 0:
         return []
-    if length < len(fibonacci_sequence):
-        return [fibonacci_sequence[length-1]]
-    if length == len(fibonacci_sequence):
-        return fibonacci_sequence
 
-    for i in range(2,length):
-        next_number = fibonacci_sequence[i-2] + fibonacci_sequence[i-1]
-        fibonacci_sequence.append(next_number)
+    result = start_sequence.copy()
 
-    return fibonacci_sequence
+    if length <= len(result):
+        return result[:length]
+    
+    for i in range(len(result), length):
+        next_number = result[i-2] + result[i-1]
+        result.append(next_number)
+    
+    return result
 
 
 print(fibonacci_sequence([0, 1], 20))
