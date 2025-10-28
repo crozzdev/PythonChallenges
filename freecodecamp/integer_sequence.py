94b8124deafc4336fb91bd78574df828eae0f647
CrozzDev <crozzdev95@outlook.com>
2025-10-27 22:13:11 -0500

    solved the integer_sequence challenge freecodecamp

---
+++ freecodecamp/integer_sequence.py
@@ -0,0 +1,4 @@
+def sequence(length: int)->str:
+    if length < 1:
+        return ""
+    return "".join([str(number) for number in range(1,length+1)])
---
+++ freecodecamp/test_integer_sequence.py
@@ -0,0 +1,7 @@
+from integer_sequence import sequence
+
+def test_sequence():
+    assert sequence(5) == "12345"
+    assert sequence(10) == "12345678910"
+    assert sequence(1) == "1"
+    assert sequence(27) == "123456789101112131415161718192021222324252627"
