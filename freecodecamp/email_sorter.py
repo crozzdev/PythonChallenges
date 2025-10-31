41837d3701e99be95de9adaa4a1700d0a49a70cc
CrozzDev <crozzdev95@outlook.com>
2025-10-30 23:54:14 -0500

    Solved the email_sorter challenge freecodecamp

---
+++ freecodecamp/email_sorter.py
@@ -0,0 +1,4 @@
+def sort(emails: list[str]) -> list:
+    return sorted(emails, key=lambda email: tuple(part.lower() for part in email.split('@')[::-1]))
+
+print(sort(["jill@mail.com", "john@example.com", "jane@example.com"]))
