0fd0557e70c47771b9ae5da15e4ce95585e06f76
crozzdev <crozzdev95@outlook.com>
2025-11-21 11:41:13 -0500

    made some improvements to my solution to the word_search challenge freecodecamp

--- freecodecamp/word_search.py
+++ freecodecamp/word_search.py
@@ -1,22 +1,21 @@
 def find_word(matrix: list[list[str]], word: str) -> list[list[int]] | None:
+    """
+    Find a word in a 2D matrix and return start and end coordinates.
+
+    Args:
+        matrix: 2D list of single lowercase letters
+        word: Word to find in the matrix
+
+    Returns:
+        [[start_row, start_col], [end_row, end_col]] or None if not found
+    """
     n_rows = len(matrix)
     n_columns = len(matrix[0])
     word_size = len(word)
-    word_list = list(word)
-
-    corners = {
-        "upper-left": (0, 0),
-        "upper-right": (0, n_rows - 1),
-        "bottom-left": (n_columns - 1, 0),
-        "bottom-right": (n_columns - 1, n_rows - 1),
-    }
 
     for i, row in enumerate(matrix):
         for j, cell in enumerate(row):
-            cell_pos = (i, j)
-
             if cell == word[0]:
-                seen_right, seen_left, seen_up, seen_down = [[cell] for _ in range(4)]
                 positions_right, positions_left, positions_up, positions_down = [
                     [[i, j]] for _ in range(4)
                 ]
@@ -24,52 +23,30 @@ def find_word(matrix: list[list[str]], word: str) -> list[list[int]] | None:
                 for k in range(1, word_size):
                     # we look to the right
                     # do we have enough cells to the right?
-                    if (
-                        cell_pos
-                        not in [corners["bottom-right"], corners["upper-right"]]
-                        and j <= n_columns - word_size
-                        and matrix[i][j + k] == word[k]
-                    ):
+                    if j <= n_columns - word_size and matrix[i][j + k] == word[k]:
                         positions_right.append([i, j + k])
-                        seen_right.append(matrix[i][j + k])
-                        if seen_right == word_list:
+                        if len(positions_right) == word_size:
                             return [positions_right[0], positions_right[-1]]
 
                     # we look to the left
                     # do we have enough cells to the left?
-                    if (
-                        cell_pos not in [corners["bottom-left"], corners["upper-left"]]
-                        and j >= word_size - 1
-                        and matrix[i][j - k] == word[k]
-                    ):
+                    if j >= word_size - 1 and matrix[i][j - k] == word[k]:
                         positions_left.append([i, j - k])
-                        seen_left.append(matrix[i][j - k])
-                        if seen_left == word_list:
+                        if len(positions_left) == word_size:
                             return [positions_left[0], positions_left[-1]]
 
                     # we look down
                     # do we have enough cells downwards?
-                    if (
-                        cell_pos
-                        not in [corners["bottom-left"], corners["bottom-right"]]
-                        and i <= n_rows - word_size
-                        and matrix[i + k][j] == word[k]
-                    ):
+                    if i <= n_rows - word_size and matrix[i + k][j] == word[k]:
                         positions_down.append([i + k, j])
-                        seen_down.append(matrix[i + k][j])
-                        if seen_down == word_list:
+                        if len(positions_down) == word_size:
                             return [positions_down[0], positions_down[-1]]
 
                     # we look up
                     # do we have enough cells upwards?
-                    if (
-                        cell_pos not in [corners["upper-left"], corners["upper-right"]]
-                        and i >= word_size - 1
-                        and matrix[i - k][j] == word[k]
-                    ):
+                    if i >= word_size - 1 and matrix[i - k][j] == word[k]:
                         positions_up.append([i - k, j])
-                        seen_up.append(matrix[i - k][j])
-                        if seen_up == word_list:
+                        if len(positions_up) == word_size:
                             return [positions_up[0], positions_up[-1]]
 
 
